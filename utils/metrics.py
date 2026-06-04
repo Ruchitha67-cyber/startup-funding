113c85b5700c9598130bff0c6556c617ae5460f1
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 15:02:28 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,5 +3,7 @@ def get_metrics(df):
     return {
         "total_startups": len(df),
         "total_funding": df["Funding Amount (M USD)"].sum(),
-        "avg_valuation": df["Valuation (M USD)"].mean()
+        "avg_valuation": df["Valuation (M USD)"].mean(),
+        "avg_revenue": df["Revenue (M USD)"].mean(),
+        "profitable_pct": df["Profitable"].mean() * 100
     }
