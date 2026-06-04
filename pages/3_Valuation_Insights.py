88df27ba29bf7ab996f8984a22ac3e8a4311e152
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:28:46 +0530

    Update 3_Valuation_Insights.py

--- pages/3_Valuation_Insights.py
+++ pages/3_Valuation_Insights.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+st.title("📈 Valuation Insights")
+
+df = load_data()
+
+fig = px.scatter(
+    df,
+    x="Revenue (M USD)",
+    y="Valuation (M USD)",
+    size="Employees",
+    color="Industry"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
