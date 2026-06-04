8388655a7cc9d4ec43b98aab28db3edcd3f9bc77
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:39:04 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,9 @@
+def preprocess_data(df):
 
+    df = df.drop_duplicates()
+    df = df.dropna()
+
+    if "Year Founded" in df.columns:
+        df["Startup Age"] = 2025 - df["Year Founded"]
+
+    return df
