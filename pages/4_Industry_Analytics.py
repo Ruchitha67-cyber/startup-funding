3300fce79b8ddc8cfc1835d279ee943517bf4c13
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:29:17 +0530

    Update 4_Industry_Analytics.py

--- pages/4_Industry_Analytics.py
+++ pages/4_Industry_Analytics.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+st.title("🏭 Industry Analytics")
+
+df = load_data()
+
+industry = (
+    df.groupby("Industry")
+    ["Valuation (M USD)"]
+    .mean()
+    .reset_index()
+)
+
+fig = px.bar(
+    industry,
+    x="Industry",
+    y="Valuation (M USD)"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
