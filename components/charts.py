bb429b210441c7f7e26dd13008c703b78a605394
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:32:20 +0530

    Update charts.py

--- components/charts.py
+++ components/charts.py
@@ -1,1 +1,23 @@
+import plotly.express as px
+import streamlit as st
 
+def funding_chart(df):
+
+    funding = (
+        df.groupby("Industry")
+        ["Funding Amount (M USD)"]
+        .sum()
+        .reset_index()
+    )
+
+    fig = px.bar(
+        funding,
+        x="Industry",
+        y="Funding Amount (M USD)",
+        title="Funding by Industry"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
