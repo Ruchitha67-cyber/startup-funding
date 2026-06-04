82b36c578a21fdf9347fca90ec8312f215e8cd89
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:32:49 +0530

    Update insights.py

--- components/insights.py
+++ components/insights.py
@@ -1,1 +1,16 @@
+def generate_insights(df):
 
+    insights = []
+
+    top_industry = (
+        df.groupby("Industry")
+        ["Valuation (M USD)"]
+        .mean()
+        .idxmax()
+    )
+
+    insights.append(
+        f"🏆 Highest valuation industry: {top_industry}"
+    )
+
+    return insights
