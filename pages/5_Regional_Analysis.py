d1ca0e0a21b8c7f87b9a7e1d1a8ea28487ff49dd
Ruchitha67-cyber <ruchithadnruchitha@gmail.com>
2026-06-04 12:29:40 +0530

    Update 5_Regional_Analysis.py

--- pages/5_Regional_Analysis.py
+++ pages/5_Regional_Analysis.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import plotly.express as px
 
+from utils.data_loader import load_data
+
+st.title("🌍 Regional Analysis")
+
+df = load_data()
+
+region = (
+    df.groupby("Region")
+    ["Valuation (M USD)"]
+    .sum()
+    .reset_index()
+)
+
+fig = px.pie(
+    region,
+    names="Region",
+    values="Valuation (M USD)"
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
